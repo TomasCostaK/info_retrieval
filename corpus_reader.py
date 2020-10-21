581aff8438a9168a6fc9b05b5c0df0efe48ab2b7
Tom1k <tomascosta@ua.pt>
2020-10-21 15:54:00 +0100

    Added CSV read and filter out empty abstracts

--- corpus_reader.py
+++ corpus_reader.py
@@ -1,4 +1,5 @@
 import re
+import pandas as pd
 
 class Reader:
     def __init__(self,file):
@@ -6,10 +7,15 @@ class Reader:
     
     def read_text(self):
         words = []
+        my_df = pd.read_csv(self.path)
+        my_df = my_df[my_df.abstract.notnull()]
+        print(my_df['abstract'])
+        """
         text = open(self.path,'r')
         for line in text: 
             words.extend(re.sub("[^a-zA-Z]+"," ",line).lower().split(" "))
-        return words
+        """
+        return ["words"]
     
     def read_complex_text(self):
         words = []
